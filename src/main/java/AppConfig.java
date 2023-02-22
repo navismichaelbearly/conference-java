90d57d5bdc3ab2c907f0541b455fe41ae7f1c87d
NBEARLYJ <nbearlyj@ford.com>
2023-02-23 02:08:59 +0530

    Configured with Setting Injection

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,22 @@
+import in.bearly.repository.HibernateSpeakerRepositoryImpl;
+import in.bearly.repository.SpeakerRepository;
+import in.bearly.service.SpeakerService;
+import in.bearly.service.SpeakerServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(name = "speakerService")
+    public SpeakerService getSpeakerService() {
+        SpeakerServiceImpl service = new SpeakerServiceImpl();
+        service.setRepository(getSpeakerRepository());
+        return service;
+    }
+
+    @Bean(name = "speakerRepository")
+    public SpeakerRepository getSpeakerRepository() {
+        return new HibernateSpeakerRepositoryImpl();
+    }
+}
--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,16 @@
 import in.bearly.service.SpeakerService;
 import in.bearly.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
-        SpeakerService service = new SpeakerServiceImpl();
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //SpeakerService service = new SpeakerServiceImpl();
+
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
     }
