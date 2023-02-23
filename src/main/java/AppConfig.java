95cb1f337d7574318d4fcf9b2ed88c92c03aafac
NBEARLYJ <nbearlyj@ford.com>
2023-02-24 00:03:01 +0530

    FactoryBean configuration

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,3 +1,4 @@
+import in.bearly.Util.CalendarFactory;
 import in.bearly.repository.HibernateSpeakerRepositoryImpl;
 import in.bearly.repository.SpeakerRepository;
 import in.bearly.service.SpeakerService;
@@ -8,10 +9,23 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.util.Calendar;
+
 @Configuration
 @ComponentScan({"in.bearly"})
 public class AppConfig {
 
+    @Bean(name= "cal")
+    public CalendarFactory calFactory(){
+        CalendarFactory factory = new CalendarFactory();
+        factory.addDays(2);
+        return factory;
+    }
+
+    @Bean
+    public Calendar cal() throws Exception {
+        return calFactory().getObject();
+    }
     /*
     @Bean(name = "speakerService")
     @Scope(value= BeanDefinition.SCOPE_SINGLETON)
--- src/main/java/in/bearly/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/in/bearly/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,14 +1,18 @@
 package in.bearly.repository;
 
 import in.bearly.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
 
+    @Autowired
+    private Calendar cal;
     @Override
     public List<Speaker> findAll() {
         List<Speaker> speakers = new ArrayList<Speaker>();
@@ -18,6 +22,7 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
         speaker.setFirstName("Blissie");
         speaker.setLastName("Fidelia");
 
+        System.out.println("cal:" +cal.getTime());
         speakers.add(speaker);
 
         return speakers;
